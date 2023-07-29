394bf9333f7b23fe45e3720789da235633044be4
jamesTanna <122881884+jamesTanna@users.noreply.github.com>
2023-07-29 09:33:24 +0530

    Update keys.mjs

--- config/keys.mjs
+++ config/keys.mjs
@@ -1,2 +1,2 @@
-let Uri = 'mongodb+srv://james:james@cluster0.z5c0rg0.mongodb.net/?retryWrites=true&w=majority';
+let Uri = 'mongodb+srv://server:server@cluster0.z5c0rg0.mongodb.net/?retryWrites=true&w=majority';
 export default Uri
